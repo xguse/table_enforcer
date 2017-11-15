648341b57d30d85dbbe3997be17fca52f69d90f9
Gus Dunn <w.gus.dunn@gmail.com>
2017-11-15 16:40:55 -0500

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ test_requirements = filter_req_paths(paths=[Path("requirements.dev.txt"),
 
 setup(
     name='table_enforcer',
-    version='0.0.1',
+    version='0.1.0',
     description="ORM-like package for defining, loading, and validating table schemas in pandas.",
     long_description=readme + '\n\n' + history,
     author="Gus Dunn",
--- table_enforcer/__init__.py
+++ table_enforcer/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Gus Dunn"""
 __email__ = 'w.gus.dunn@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
