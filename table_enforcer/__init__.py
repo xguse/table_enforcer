6986ad6e0a5edc84d32a73218b2c4817b6b0fe96
Gus Dunn <w.gus.dunn@gmail.com>
2017-11-17 11:11:23 -0500

    Bump version: 0.1.1 → 0.1.2

--- table_enforcer/__init__.py
+++ table_enforcer/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Gus Dunn"""
 __email__ = 'w.gus.dunn@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from table_enforcer.table_enforcer import Enforcer, Column  # noqa: F401
