c36e5d978e0e9eab88b81cc96a530223ef41524f
Gus Dunn <w.gus.dunn@gmail.com>
2018-01-26 11:45:52 -0500

    table_enforcer.py -> main_classes.py

--- table_enforcer/__init__.py
+++ table_enforcer/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
 """Top-level package for Table Enforcer."""
 
 __author__ = """Gus Dunn"""
 __email__ = 'w.gus.dunn@gmail.com'
 __version__ = '0.1.2'
 
-from table_enforcer.table_enforcer import Enforcer, Column  # noqa: F401
+from table_enforcer.main_classes import Enforcer, Column  # noqa: F401
