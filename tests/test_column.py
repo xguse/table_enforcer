7954aec5923c6d226cae80b7be2cc4ceee491d9d
Gus Dunn <w.gus.dunn@gmail.com>
2018-02-07 10:48:33 -0500

    test_column: fix typos and style

--- tests/test_column.py
+++ tests/test_column.py
@@ -1,13 +1,14 @@
-"""Test the unit: Enforcer."""
+"""Test the unit: Column."""
 import pytest
 from .conftest import col4, col4_no_recoders, source_table  # noqa: F401
 import table_enforcer.errors as e
 
 
+# TODO: run the actual init in this function
 def test_init(col4):
     assert col4.name == 'col4'
     assert col4.dtype == str
-    assert col4.unique == False  # noqa: E712
+    assert col4.unique is False  # noqa: E712
 
 
 def test_column(col4, col4_no_recoders, source_table):
