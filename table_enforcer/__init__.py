2045fab4ee67404b81efc1b8582daf9df3fc5874
Gus Dunn <w.gus.dunn@gmail.com>
2018-02-07 15:55:44 -0500

    import all from main_classes

--- table_enforcer/__init__.py
+++ table_enforcer/__init__.py
@@ -5,4 +5,4 @@ __author__ = """Gus Dunn"""
 __email__ = 'w.gus.dunn@gmail.com'
 __version__ = '0.2.0'
 
-from table_enforcer.main_classes import Enforcer, Column  # noqa: F401
+from table_enforcer.main_classes import *  # noqa: F401,F403
