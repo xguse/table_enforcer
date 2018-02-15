b965589eee400887825de41597cdb99dbb0cc4d0
Gus Dunn <w.gus.dunn@gmail.com>
2018-02-15 11:28:27 -0500

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ test_requirements = filter_req_paths(paths=[Path("requirements.dev.txt"),
 
 setup(
     name='table_enforcer',
-    version='0.4.1',
+    version='0.4.2',
     description="ORM-like package for defining, loading, and validating table schemas in pandas.",
     long_description=readme + '\n\n' + history,
     author="Gus Dunn",
